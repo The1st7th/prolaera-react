9389b9276d3bd69fded41e12d719e97273cd3802
andrew lupton <aflupton@gmail.com>
2018-08-31 15:00:02 -0700

    add button changes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,13 +5,15 @@ class Button extends React.Component {
   render() {
     const { text = '', link } = this.props;
     return (
-      <div>
-        <div align="center" style={{ textAlign: 'center', height: '100%', width: '100%' }}>
-          <a href={link}>
-            <button style={Colorpick(this.props.color)}>{text}</button>
-          </a>
-        </div>
-      </div>
+      <Box>
+        <Item>
+          <div align="center" style={{ textAlign: 'center', height: '100%', width: '100%' }}>
+            <a href={link} style={Colorpick(this.props.color)}>
+              {text}
+            </a>
+          </div>
+        </Item>
+      </Box>
     );
   }
 }
