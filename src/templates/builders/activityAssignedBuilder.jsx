18366cdd855077ae716f175a65db99e4ce09fbe5
andrew lupton <aflupton@gmail.com>
2018-09-10 15:12:45 -0700

    add subheader changes

--- src/templates/builders/activityAssignedBuilder.jsx
+++ src/templates/builders/activityAssignedBuilder.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Email, renderEmail } from 'react-html-email';
+// import { renderEmail } from 'react-html-email';
 import ActivityInfo from '../../components/activity/activityInfo';
+import Email from '../../components/newemail';
 import Footer from '../../components/footer';
 import buildHeader from '../../components/header';
 import SubHeader from '../../components/subHeader';
 import css from '../templateCSS';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 const activityAssignedEmail = async (activity, user, imageUrl) => {
   try {
@@ -21,5 +23,11 @@ const activityAssignedEmail = async (activity, user, imageUrl) => {
     throw error;
   }
 };
+
+function renderEmail(emailComponent) {
+  var doctype =
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+  return doctype + renderToStaticMarkup(emailComponent);
+}
 
 export default activityAssignedEmail;
